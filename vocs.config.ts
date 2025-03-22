e036f8a79ae9fcb499e0717a0f1d72172535233e
Paul Mollahan <paul.mollahan.digirati@gmail.com>
2025-03-22 19:35:00 +0000

    Update vocs.config.ts
    Updating menu

--- vocs.config.ts
+++ vocs.config.ts
@@ -8,6 +8,10 @@ export default defineConfig({
       link: '/getting-started',
     },
     {
+      text: 'What you'll need for the workshop',
+      link: '/what-you-will-need',
+    },
+    {
       text: 'Example',
       link: '/example',
     },
