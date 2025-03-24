c79a7e9755f5a09ed15273258075fb31f86c2925
Stephen Fraser <stephen.fraser@digirati.com>
2025-03-24 13:20:36 +0000

    Theming

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,27 +1,37 @@
-import { defineConfig } from 'vocs'
+import { defineConfig } from "vocs";
 
 export default defineConfig({
-  title: 'Change this to workshop title',
-  sidebar: [
-     {
-      text: 'What you need for the workshop',
-      link: '/what-you-will-need',
-    },
-    {
-      text: 'IIIF content to Explore',
-      link: '/examples-of-iiif-to-explore',
-    },
-    {
-      text: 'Getting Started',
-      link: '/getting-started',
-    },
-    {
-      text: 'Manifest Editor',
-      link: '/manifest-editing',
-    },
-     {
-      text: 'Exhibition building',
-      link: '/exhibition-building',
-    },
-  ],
-})
+	title: "Change this to workshop title",
+	theme: {
+		accentColor: {
+			textAccent: { light: "#D75281", dark: "white" },
+			backgroundAccent: { light: "#D75281", dark: "#D75281" },
+			backgroundAccentHover: { light: "#D75281", dark: "#D75281" },
+			backgroundAccentText: { light: "white", dark: "white" },
+			borderAccent: { light: "#D75281", dark: "#D75281" },
+			textAccentHover: { light: "#D75281", dark: "#D75281" },
+		},
+	},
+	sidebar: [
+		{
+			text: "What you need for the workshop",
+			link: "/what-you-will-need",
+		},
+		{
+			text: "IIIF content to Explore",
+			link: "/examples-of-iiif-to-explore",
+		},
+		{
+			text: "Getting Started",
+			link: "/getting-started",
+		},
+		{
+			text: "Manifest Editor",
+			link: "/manifest-editing",
+		},
+		{
+			text: "Exhibition building",
+			link: "/exhibition-building",
+		},
+	],
+});
